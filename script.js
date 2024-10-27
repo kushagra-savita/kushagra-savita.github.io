2bca4ebecadc3494b0b16fc895d1703f6d6f19af
kushagra-savita <kushagrasavita@gmail.com>
2024-10-27 15:16:03 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll for internal links (if navigation is added in the future)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
